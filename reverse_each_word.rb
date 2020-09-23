e8e1ed39bc1a7609c74f9e1b4bcc8adb70c16cbf
Allison Ricker <alliricker@gmail.com>
2020-09-23 18:47:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(words)
   new_array = words.split(/ /)
 new_array.each do |word|
-  new_array << ("#{word}").reverse
+  new_array << word.reverse
   end
   new_array.join(' ')
 end
